418cf19704a65eb8ce98fa5ef9ec5e15d3df7898
Piotr Podbielski <kontakt@piotrpodbielski.pl>
2019-10-13 01:39:44 +0200

    Complete algolia accidents search mechanism

--- src/app/services/nearestaccidents/nearest-accidents.service.ts
+++ src/app/services/nearestaccidents/nearest-accidents.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
 import * as AlgoliaSearch from 'algoliasearch';
 import * as turf from '@turf/turf';
-import {Observable} from "rxjs";
-import {Geometry} from "../../interfaces/osrm";
+import {Index} from "algoliasearch";
 
 
 @Injectable({
@@ -10,32 +9,36 @@ import {Geometry} from "../../interfaces/osrm";
 })
 export class NearestAccidentsService {
 
-  private index: any;
+  private index: Index;
 
   constructor() {
     this.index = AlgoliaSearch('8VD8ZKJMEV', 'f313d81e1bc45b25250c21d383b1b889').initIndex('dev_name');
   }
 
-  findNearestAccidents(geometry: Geometry) {
-    // const result = this.index.search({
-    //   hitsPerPage: 10,
-    //   page: 0,
-    //   analytics: false,
-    //   getRankingInfo: true,
-    //   aroundLatLng: '51.5074,0.1278',
-    //   aroundRadius: 10000,
-    // });
-    //
-    // console.log(result);
-    return new Promise<string>( (resolve, reject) => {
-      resolve(this.getPolygonFromPoints(geometry));
+  findNearestAccidents(coordinates: number[][]) {
+    const simplified = this.getPolygonFromPoints(coordinates);
+    const coordinatesPolygon = simplified.geometry.coordinates;
+
+    const coordinatesPolygonSqueezed = [];
+    coordinatesPolygon[0].forEach((val) => {
+      coordinatesPolygonSqueezed.push(val[1]);
+      coordinatesPolygonSqueezed.push(val[0]);
     });
+
+    const params = {
+      hitsPerPage: 1000,
+      page: 0,
+      analytics: false,
+      getRankingInfo: true,
+      insidePolygon: [coordinatesPolygonSqueezed],
+      aroundRadius: 10000,
+    };
+
+    return this.index.search(params);
   }
 
-  getPolygonFromPoints(geometry: Geometry) {
-    // const buffered = turf.buffer(geometry, 1, {units: 'kilometers'});
-    //
-    // console.log(buffered);
-    return 'ddd';
+  getPolygonFromPoints(coordinates: number[][]) {
+    const line = turf.lineString(coordinates);
+    return turf.simplify(turf.buffer(line, 0.05, {units: 'kilometers'}), {tolerance: 0.005, highQuality: false});
   }
 }
